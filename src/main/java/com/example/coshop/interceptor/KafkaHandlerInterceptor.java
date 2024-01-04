eddc3da72553981a754a6c1d826c12b1badabf28
Jeonghoon2 <ukidd12@naver.com>
2024-01-04 16:40:16 +0900

    KafkaHandlerInterceptor - log를 카프카로 전송 부분 구현 중

---
+++ src/main/java/com/example/coshop/interceptor/KafkaHandlerInterceptor.java
@@ -0,0 +1,34 @@
+package com.example.coshop.interceptor;
+
+import com.example.coshop.configuration.KafkaProducerConfig;
+import com.example.coshop.dto.log.LogDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import static com.example.coshop.constants.HeaderConstant.KEY_RESPONSE_HEADER_TRACE_ID;
+import static com.example.coshop.constants.KafkaConstant.KAFKA_TOPIC_NAME;
+
+public class KafkaHandlerInterceptor implements HandlerInterceptor {
+
+    private final KafkaProducerConfig producerConfig = new KafkaProducerConfig();
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+
+        /**
+         * Log TraceId를 기준으로 SharedData에서 Message를 가져온다.
+         *  */
+        LogDto message = LoggingHandlerInterceptor.sharedData.get(response.getHeader(KEY_RESPONSE_HEADER_TRACE_ID));
+        producerConfig.kafkaTemplate().send(new ProducerRecord<>(KAFKA_TOPIC_NAME,message.toString()));
+
+        LoggingHandlerInterceptor.sharedData.remove(response.getHeader(KEY_RESPONSE_HEADER_TRACE_ID));
+    }
+
+    public void init(){
+
+    }
+
+}
