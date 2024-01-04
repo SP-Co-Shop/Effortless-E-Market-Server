be32404fa939da8be9b89ec5cf26003c919def16
Jeonghoon2 <ukidd12@naver.com>
2024-01-04 17:01:02 +0900

    constants

--- src/main/java/com/example/coshop/constants/KafkaConstant.java
+++ src/main/java/com/example/coshop/constants/KafkaConstant.java
@@ -2,7 +2,7 @@ package com.example.coshop.constants;
 
 public class KafkaConstant {
 
-    public static final String KAFKA_BOOTSTRAP_SERVER = "my-kafka:9092";
-    public static final String SPRING_LOG_TO_KAFKA_TOPIC = "test-kafka";
+    public static final String KAFKA_BOOTSTRAP_SERVER           = "my-kafka:9092";
+    public static final String KAFKA_TOPIC_NAME                 = "test-kafka";
 
 }
