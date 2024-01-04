33d16bf02b2b40fea3b5894a60337d53ffda6753
Jeonghoon2 <ukidd12@naver.com>
2024-01-04 17:05:30 +0900

    Kafka Producer 구현

---
+++ src/main/java/com/example/coshop/kafka/KafkaProducer.java
@@ -0,0 +1,18 @@
+package com.example.coshop.kafka;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String topic, String message){
+
+        kafkaTemplate.send(topic, message);
+
+    }
+}
